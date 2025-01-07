6d72c6a08316fc36d388aa2063ca427412b67df5
Joost <2006joost@gmail.com>
2025-01-07 14:21:48 +0100

    working on layout

--- FlutterProjects/car_app/lib/main.dart
+++ FlutterProjects/car_app/lib/main.dart
@@ -1,44 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() => runApp(MaterialApp(
+  home: CarApp(),
+  debugShowCheckedModeBanner: false,
+));
 
-  home: Home()
-  ));
-class Home extends StatelessWidget {
-  const Home({super.key});
-
+class CarApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('My First Fire Application'),
-          centerTitle: true,
-          backgroundColor: Colors.blue[900],
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        backgroundColor: Colors.grey[900],
+        title: Text(
+          style: TextStyle(
+            fontFamily: 'Poppins',
+            color: Colors.white,
+            shadows: [
+              Shadow(
+                offset: Offset(2.0, 2.0),
+                blurRadius: 10.0,
+                color: Colors.black,
+              )
+            ]
+          ),
+            'Escort'
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('hello'),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('test')
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // Add an image widget here
+          Padding(
+            padding: EdgeInsets.only(top: 40.0,bottom: 50), // Adjust padding as needed
+            child: Image.asset(
+              'assets/FordEscort.png', // Replace with your image path
             ),
-            Container(
-              color: Colors.cyan,
-              padding: EdgeInsets.all(30.0),
-              child : Text('Inside Container'),
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          child: Text('Click'),
-          backgroundColor: Colors.grey,
-        )
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                SvgPicture.asset(
+                  'assets/roadster-fill.svg',
+                  color: Color(0xFF8A8C8B),
+                ),
+                SvgPicture.asset(
+                  'assets/lock-fill.svg',
+                  color: Color(0xFF8A8C8B),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
-
-
-
