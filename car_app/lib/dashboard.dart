11a85a49eb54d29188b7b589f1a4c69b87431345
Joost <113507203+JoostKraan@users.noreply.github.com>
2025-03-02 22:30:29 +0100

    Organised inkscape folder and worked on dashboard
    made basic layout for dashboard, made gear icons, fixed rim icon (Symmetry)
    Made vehicle sillouette top view with tire icons

--- car_app/lib/dashboard.dart
+++ car_app/lib/dashboard.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -16,19 +18,112 @@ class Dashboard extends StatelessWidget {
         ),
         borderRadius: BorderRadius.circular(8),
       ),
-      child: Column(
+      child: Stack(
         children: [
-          Row(
+          Column(
             children: [
-              SvgPicture.asset(
-                  color: Colors.white,
-                  width: 50,
-                  'assets/Ford_Escort_Rim.svg'),
+              SizedBox(height: 10),
+              Row(
+                children:  [
+                  SizedBox(width: 10),
+                  Image.asset(
+                      width: 45,
+                      'assets/gears/Neutral_Gear.png'
+                  ),
+                ]
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  SizedBox(width: 10),
+                  SvgPicture.asset(
+                      width: 45,
+                      color: Colors.white,
+                      'assets/Ford_Escort_Rim.svg'),
+                  SizedBox(width: 10),
+                  Text(
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        color: Colors.white
+                      ),
+                      '20 km/h')
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  SizedBox(width: 10),
+                  Text(
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                      'RPM stuff here')
+                ],
+                
+              )
             ],
-          )
 
+          ),
+          Positioned(
+            top: 10,
+            right: 30,
+            child: SvgPicture.asset(
+              'assets/escort_sillouette2.svg',
+              width: 75,
+              color: const Color(0xFF8A8C8B),
+            ),
+          ),
+          Positioned(
+            right: 20,
+            top: 20,
+            child: SizedBox(
+              width: 10,
+              height: 30,
+              child: SvgPicture.asset(
+                'assets/tire.svg',
+                width: 10,
+                color: const Color(0xFF8A8C8B),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 283,
+            top: 20,
+            child: SizedBox(
+              width: 10,
+              height: 30,
+              child: SvgPicture.asset(
+                'assets/tire.svg',
+                color: const Color(0xFF8A8C8B),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 280,
+            top: 135,
+            child: SizedBox(
+              width: 10,
+              height: 30,
+              child: SvgPicture.asset(
+                'assets/tire.svg',
+                color: const Color(0xFF8A8C8B),
+              ),
+            ),
+          ),
+          Positioned(
+            right: 17,
+            top: 135,
+            child: SizedBox(
+              width: 10,
+              height: 30,
+              child: SvgPicture.asset(
+                'assets/tire.svg',
+                color: const Color(0xFF8A8C8B),
+              ),
+            ),
+          ),
         ],
-      )
+      ),
     );
   }
 }
